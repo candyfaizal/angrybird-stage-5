c1693ed405bc28928d2e6ba6470725f084433450
candyfaizal <royalfaizal4@gmail.com>
2021-05-27 19:54:40 +0530

    we launch the bird

--- SlingShot.js
+++ SlingShot.js
@@ -10,12 +10,18 @@ class SlingShot{
         this.SlingShot= Constraint.create(options);
         World.add(world, this.SlingShot);
     }
-
+       fly(){
+           this.SlingShot.bodyA=null
+       }
       display(){
+          if(this.SlingShot.bodyA!==null)
+          {
             var pointA = this.SlingShot.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
+          }
+           
         
     }
     
